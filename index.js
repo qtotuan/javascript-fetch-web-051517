27f0c7eda9bc2c5c2c30c8377301c45ec3bcd041
qtotuan <quynh.totuan@gmail.com>
2017-07-05 10:19:00 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
-const app = "I don't do much.";
+// const app = "I don't do much.";
+//
+//
+// let xhr = new XMLHttpRequest()
+// xhr.open('GET', 'https://api.github.com/repos/jquery/jquery/commits')
+//
+// xhr.onload = function() {
+//   console.log(xhr.response)
+// }
+//
+// xhr.onerror = function() {
+//   console.log('Booo')
+// }
+//
+// xhr.send()
+
+const token = '9988a3c3a202729d02132294f40c9a7d84ffbae3'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+})
+  .then(res => res.json())
+  .then(json => console.log(json))
